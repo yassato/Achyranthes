247138c990da857d990ee792f4adfa25e99f32ba
Yasuhiro SATO <yasuhirosato@Yasuhiros-MacBook-Pro.local>
2026-01-04 22:48:36 +0900

    proofread

--- script/stat.R
+++ script/stat.R
@@ -3,22 +3,26 @@ library(lme4)
 library(ordinal)
 library(VGAM)
 
+# multinomial GLM for Figure 2
 d0 = read.csv("./data/two_sites_data.csv")
 glm0 = vglm(cbind(Lepidoptera,Hymenoptera,Diptera,Coleoptera)~site+time,family=multinomial,data=d0)
 drop1(glm0,test="LRT")
 
+# binomial and Poisson GLMMs for Figure 3a
 d1 = read.csv("./data/frequency_data.csv")
-glmm1 = glmer(cbind(in.,out)~type+(1|date),family=binomial,data=d1)
+glmm1 = glmer(cbind(in.,out)~type+(1|date),family="binomial",data=d1)
 drop1(glmm1,test="Chisq")
 
 glmm11 = glmer(total~type+(1|date),family="poisson",data=d1)
 drop1(glmm11,test="Chisq")
 
+# Gamma GLMM for Figure 3b
 d2 = read.csv("./data/time_data.csv")
 d22 = gather(d2,c("honeybee","butterfly"),key="type",value="sec")
 glmm22 = glmer(sec~type+(1|date),family="Gamma",data=d22)
 drop1(glmm22,test="Chisq")
 
+# ordered multinomial GLMM for Figure 3c vs. d
 d3 = read.csv("./data/distance_data.csv")
 d32 = gather(d3,c("honeybee","butterfly"),key="type",value="cm")
 d32$cm[d32$cm==">100"] = 100
